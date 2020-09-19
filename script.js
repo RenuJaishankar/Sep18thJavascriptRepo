2df5f158abf9f5a80298d6381285b7c9a06550ee
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-09-18 21:47:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var answer1="It's alright";
+var answer2="His name is 'John'";
+var answer3='His name is "John"';
+let a=document.getElementById("demo");
+a.innerHTML=answer1+ "<br>" + answer2+ "<br>" +answer3;
